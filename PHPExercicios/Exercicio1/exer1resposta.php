3adf8708c21797abb399550fe040b0452c2823bd
Thiagoads457 <thiagoads457@gmail.com>
2024-04-02 19:44:18 -0300

    1944

---
+++ PHPExercicios/Exercicio1/exer1resposta.php
@@ -0,0 +1,9 @@
+<?php
+    $valor = $_POST['valor'];
+    if ($valor > 0)
+        echo "Valor Positivo";
+    elseif ($valor < 0)
+        echo "Valor negativo";
+    else 
+        echo "Valor igual a zero!";
+   

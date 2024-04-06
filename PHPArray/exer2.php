a66a4ff9b0f74d1d06ee93496a0f0fead97947cd
Thiagoads457 <thiagoads457@gmail.com>
2024-04-06 10:22:20 -0300

    Revert "Update exer2.php"
    This reverts commit 344f121dbf92297d79e011212fffac4cb60e0e37.

--- PHPArray/exer2.php
+++ PHPArray/exer2.php
@@ -14,14 +14,5 @@
         ?>
         <button type="submit">Ok</button>
     </form>
-    <?php
-        if (isset($_POST)){
-            if (isset ($_POST['valores'])){
-                $vetor = $_POST['valores'];
-                var_dump($vetor);
-            }
-        }
-    ?>
-
 </body>
 </html>

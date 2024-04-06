c2bd6ae3dd3a4b516348c7e58440f7cd2317b73c
Thiagoads457 <thiagoads457@gmail.com>
2024-04-05 21:41:45 -0300

    0504

--- PHPArray/processar_media.php
+++ PHPArray/processar_media.php
@@ -4,9 +4,13 @@
     $n2 = $_POST['n2'];
     $n3 = $_POST['n3'];
     $n4 = $_POST['n4'];
+    $faltas = $_POST['faltas'];
     $media = ($n1 + $n2 + $n3 +$n4) / 4;
-    if ($media >= 6){
+    if (($media >= 6 ) && ($faltas <=10)){
         echo "Aprovado!";
     }else{
         echo "Reprovado";
     }
+
+    $vetor = array(10, 20, 30);
+    echo $vetor[1];

55937363c360248866364b102f0c32d6ed92798f
Thiagoads457 <thiagoads457@gmail.com>
2024-04-13 11:21:05 -0300

    1121

---
+++ PHPExercicios2/Exercicio3/exer3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio3</title>
+</head>
+<body>
+    <h2>Digite 10 valores numéricos: </h2>
+    <form action="exer3resposta.php" method="post">
+        <?php 
+            for ($i=0;$i<3;$i++) {
+                echo '<input type="number" name="numeros[]"><br>';
+            }
+            echo '<label>Digite um número multiplicador: </label>&nbsp;
+                <input type="number" name="multiplica">';
+        ?>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ PHPExercicios2/Exercicio4/exer4.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio4</title>
+</head>
+<body>
+    <h2>"Para obter o nome do mês correspondente digite um número de 1 a 12: "</h2>
+    <form action="exer4resposta.php" method="post">
+        <input type="number" name="mes">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
